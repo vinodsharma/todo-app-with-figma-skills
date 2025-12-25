1108ad609511c6cf0e2037bbe5d4dc026b7620cc
Vinod Sharma <vinodsharma.mimit@gmail.com>
2025-12-25 16:01:05 +0530

    feat: configure NextAuth.js with credentials and Google providers

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,14 +1,16 @@
 'use client';
 
 import { ThemeProvider } from 'next-themes';
+import { SessionProvider } from 'next-auth/react';
 import { Toaster } from '@/components/ui/sonner';
 
-// Note: SessionProvider will be added when NextAuth is installed in Milestone 2
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
-      <Toaster />
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+        <Toaster />
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
